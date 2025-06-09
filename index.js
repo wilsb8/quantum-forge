0152458eb1c3dfff95f0822f26bb9fd6f987a0f6
wilsb8 <wilsb8@gmail.com>
2025-06-09 13:16:44 -0700

    first press

---
+++ index.js
@@ -0,0 +1,8 @@
+const http = require('http');
+require('dotenv').config();
+// const app = require('./app/app');
+const port = process.env.PORT || 3000;
+
+http.createServer().listen(process.env.port, ()=>{
+    console.log(`Server here, I am fully operational and all my circuits are listening on port: ${process.env.port}.`);
+});
